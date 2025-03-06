16ac73bde2de5347a18ce47925e87dec23779c83
Jérémy <76524822+Pinkoune@users.noreply.github.com>
2025-03-06 11:53:47 +0100

    Init mainFrame & assets

--- Fortech-iOS/ContentView.swift
+++ Fortech-iOS/ContentView.swift
@@ -1,21 +1,31 @@
 //
 //  ContentView.swift
-//  Fortech-iOS
+//  fortech-iOS
 //
-//  Created by Jérémy Barcelo on 06/03/2025.
+//  Created by Jérémy Barcelo on 25/02/2025.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            EllipticalGradient(colors:[Color.bgLightBlue, Color.bgDarkBlue], center: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, startRadiusFraction: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, endRadiusFraction: 0.5)
+            VStack {
+                Image("FortechTitle")
+                    .resizable()
+                    .frame(width: 300, height: 100.0)
+                    .padding(.bottom, 250.0)
+                Button(action: {}, label: {
+                    Image("ConnectionButton")
+                        .resizable()
+                        .frame(width: 250, height: 100)
+                })
+            }
+            .padding()
+            
         }
-        .padding()
+        .ignoresSafeArea()
     }
 }
 
